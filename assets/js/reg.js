6939954ea5d0cfcef55a25b99e785e509316f8ab
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-06-09 19:04:56 +0530

    Added check for forAdded check for formm

--- assets/js/reg.js
+++ assets/js/reg.js
@@ -7,9 +7,11 @@ function sendReg() {
 		url: "https://festember.com/reels/reg.php",
 		success: function(data) {
 			alert("Success");
+			document.getElementById('form-button').innerHTML = "<i class='fa fa-check-circle'></i>";
 		},
 		error: function(data){
-		    alert(data);
+		    //alert(data);
+			document.getElementById('form-button').innerHTML = "<i class='fa fa-check-close'></i>";
 		    console.log(data);
 		}
 	});
