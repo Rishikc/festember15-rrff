ae5d4189d8dad2f289eb5b8613e01f18c52af404
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-06-09 09:29:58 -0400

    Reg bug fix.

--- assets/js/reg.js
+++ assets/js/reg.js
@@ -2,7 +2,7 @@ function sendReg() {
 	console.log(document.getElementById('email').value);
 	$.ajax({
 		type: "POST",
-		dataType: "jsonp",
+		//dataType: "jsonp",
 		data: {'email': document.getElementById('email').value },
 		url: "https://festember.com/reels/reg.php",
 		success: function(data) {
